f635d61a2032341aca4971bad4c516bb483969cf
mossypillow <aprivee@gmail.com>
2019-05-04 07:29:32 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+import urllib3
+
+# Use Airship API to pull a report in CSV format.
+# "Write a program that returns data as a CSV file and Timestamp, Username, Event Name, Numbers of records recorded, Event ID"
+
+r = http.request('GET')
