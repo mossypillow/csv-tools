70b09e1bcca9cc09015dd2a6ea1c71f2698a9770
mossypillow <aprivee@gmail.com>
2019-05-05 19:28:57 -0700

    Open file working

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 #!/bin/python3
-import urllib3
+import requests
+import csv
+import json
+
+#import certifi
 
 # Use Airship API to pull a report in CSV format.
 # "Write a program that returns data as a CSV file and Timestamp, Username, Event Name, Numbers of records recorded, Event ID"
 
-r = http.request('GET')
+#http = urllib3.PoolManager(ca_certs=certifi.where())
+#r = http.request('Fetch', 'https://jsonplaceholder.typicode.com/todos/1')
+#rjson = json(r)
+# for thing in rjson:
+#    print(thing)
+# print(r.status)
+
+
+raw_request = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+export_parsed = requests.compat.json.loads(raw_request.text)
+
+with open('report_export.csv', 'w', newline='') as file:
+
+    for thing in export_parsed:
+        file.write(thing)
